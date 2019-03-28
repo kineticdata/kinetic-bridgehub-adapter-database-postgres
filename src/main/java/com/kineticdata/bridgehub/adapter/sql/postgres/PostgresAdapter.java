c6edabad8a4b81c65f5ba100beac913b1a547a68
Austin <austin.peters@kineticdata.com>
2019-03-28 16:29:55 -0500

    Removed unnecessary buildPaginatedStatement method.

--- src/main/java/com/kineticdata/bridgehub/adapter/sql/postgres/PostgresAdapter.java
+++ src/main/java/com/kineticdata/bridgehub/adapter/sql/postgres/PostgresAdapter.java
@@ -1,17 +1,10 @@
 package com.kineticdata.bridgehub.adapter.sql.postgres;
 
 import com.kineticdata.bridgehub.adapter.BridgeError;
-import com.kineticdata.bridgehub.adapter.BridgeRequest;
 import com.kineticdata.bridgehub.adapter.sql.SqlAdapter;
-import com.kineticdata.bridgehub.adapter.sql.SqlQualification;
-import com.kineticdata.bridgehub.adapter.sql.SqlQualificationParameter;
-import com.kineticdata.bridgehub.adapter.sql.SqlQualificationParser;
 import com.kineticdata.commons.v1.config.ConfigurableProperty;
 import com.kineticdata.commons.v1.config.ConfigurablePropertyMap;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
 import java.util.Map;
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.LoggerFactory;
@@ -92,95 +85,13 @@ public class PostgresAdapter extends SqlAdapter {
             ADAPTER_CLASS,
             "jdbc:postgresql://"+
                 properties.getValue(Properties.SERVER)+":"+
-                properties.getValue(Properties.PORT)+";"+
-                "databaseName="+properties.getValue(Properties.DATABASE_NAME)+";"+
-                StringUtils.defaultString(properties.getValue(Properties.APPEND_CONNECTION), ""),
+                properties.getValue(Properties.PORT)+"/"+
+                properties.getValue(Properties.DATABASE_NAME)+
+                (StringUtils.isEmpty(properties.getValue(Properties.APPEND_CONNECTION)) ? 
+                    "" : "?" + properties.getValue(Properties.APPEND_CONNECTION)),
             properties.getValue(Properties.USERNAME),
             properties.getValue(Properties.PASSWORD)
         );
     }
 
-    @Override
-    protected PreparedStatement buildPaginatedStatement(
-        Connection connection,
-        BridgeRequest request,
-        Long offset,
-        Long pageSize
-    ) throws BridgeError, SQLException {
-        // Build the list of columns to retrieve from the field string
-        String columns = request.getFieldString();
-        // If the field string was not provided, default it to all columns
-        if (StringUtils.isBlank(columns)) {columns = "*";}
-
-        // Build up the SQL WHERE clause
-        SqlQualification qualification = SqlQualificationParser.parse(request.getQuery());
-        // Build the SQL ORDER BY clause (validating that only the requested
-        // fields are used in the column list and that there is no attempt at
-        // injection).
-        String order = null;
-        if (StringUtils.isNotBlank(request.getMetadata("order"))) {
-            order = SqlQualificationParser.buildOrderByClause(request.getFields(), request.getMetadata("order"));
-        } else if(!"*".equals(columns)) {
-            order = SqlQualificationParser.buildOrderByClause(request.getFields(), columns);
-        }
-
-        // Build the main statement string
-        StringBuilder statementString = new StringBuilder();
-
-        // DB2 Does not support the OFFSET and LIMIT keyworsds, but a similar
-        // result can be accomplished by wrapping a query in another
-        // "pagination" query.
-        //   * offset means how many row s to skip
-        //   * 0 implies show the first row.
-        //   * If pageSize is zero, don't use pagination.
-        if (offset >= 0 && pageSize > 0 && !"*".equals(columns)) {
-            offset = (offset>0?++offset : offset);
-            pageSize = (offset>0? offset-1+pageSize: pageSize);
-
-            statementString.append("SELECT IDENTITY(int,1,1) AS RowNumber, * INTO #temp");
-            statementString.append(" FROM ").append(request.getStructure());
-            statementString.append(" WHERE ").append(qualification.getParameterizedString());
-            statementString.append(" ORDER BY ").append(order);
-            statementString.append(";");
-            statementString.append("SELECT ").append(columns);
-            statementString.append(" FROM #Temp ");
-            statementString.append(" WHERE RowNumber BETWEEN ").append(offset).append(" AND ").append(pageSize);
-            statementString.append(" ORDER BY RowNumber DROP TABLE #Temp");
-        } else {
-            if (offset >= 0 && pageSize > 0){
-                logger.warn("Unable to apply offset and pageSize when "+
-                    "retrieve * columns, defaulting to returning all rows");
-            }
-            // Build a request without pagination
-            statementString.append("SELECT ").append(columns);
-            statementString.append(" FROM ").append(request.getStructure());
-            statementString.append(" WHERE ").append(qualification.getParameterizedString());
-            if (order != null) {
-                statementString.append(" ORDER BY ").append(order);
-            }
-        }
-
-        // Prepare the statement
-        logger.debug("Preparing Query");
-        logger.debug("  "+statementString);
-        PreparedStatement statement = connection.prepareStatement(statementString.toString());
-        for (SqlQualificationParameter parameter : qualification.getParameters()) {
-            // Retrieve the parameter value
-            String parameterValue = request.getParameter(parameter.getName());
-            // If there is a reference to a parameter that was not passed
-            if (parameterValue == null) {
-                throw new BridgeError("Unable to parse qualification, "+
-                    "the '"+parameter.getName()+"' parameter was "+
-                    "referenced but not provided.");
-            }
-            
-            // Log each parameter value in the query
-            logger.trace("  "+ Integer.toString(parameter.getIndex()+1) + " (" +parameter.getName()+") : "+parameterValue);
-            // Set the value for the parameter in the SQL statement.
-            statement.setObject(parameter.getIndex(), parameterValue);
-        }
-
-        // Return the statement
-        return statement;
-    }
 }
